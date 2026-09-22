d430b1b8ef6ae6bda69ce23e2b5c03f7f1e9c952
Ищенко Дмитрий Александрович <daischenko@vtb.ru>
2026-09-22 15:11:57 +0300

    Убрал атомарную смену статуса через ReentrantReadWriteLock

--- payment-provider-service/src/main/java/com/example/service/impl/WebhookServiceImpl.java
+++ payment-provider-service/src/main/java/com/example/service/impl/WebhookServiceImpl.java
@@ -14,8 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Slf4j
 @Service
@@ -27,7 +25,6 @@ public class WebhookServiceImpl implements WebhookService {
     private final TransactionService transactionService;
     private final PayoutService payoutService;
     private final ObjectMapper objectMapper;
-    private final ReadWriteLock rwl = new ReentrantReadWriteLock();
 
     @Override
     public void updatePayout(StatusUpdate request) {
@@ -37,10 +34,21 @@ public class WebhookServiceImpl implements WebhookService {
                 );
         Status requestStatus = Status.fromRequest(request.getStatus());
 
-        if (updatePayoutStatus(payout, requestStatus)) {
-            Webhook webhook = createWebhook(EventType.PAYOUT, request);
-            webhookRepository.save(webhook);
+        Status payoutStatus = payout.getStatus();
+
+        if (payout.isFinalStatus()) {
+            if (payoutStatus.equals(requestStatus)) {
+                log.info("Выплата уже находится в статусе {}", payoutStatus);
+                return;
+            }
+            throw new BadRequestException(
+                    String.format("Не подходящий статус %s для выплаты с ID: %s ", requestStatus, payout.getId())
+            );
         }
+
+        payout.setStatus(requestStatus);
+        Webhook webhook = createWebhook(EventType.PAYOUT, request);
+        webhookRepository.save(webhook);
     }
 
     @Override
@@ -51,85 +59,22 @@ public class WebhookServiceImpl implements WebhookService {
                 );
         Status requestStatus = Status.fromRequest(request.getStatus());
 
-        if (updateTransactionStatus(transaction, requestStatus)) {
-            Webhook webhook = createWebhook(EventType.TRANSACTION, request);
-            webhookRepository.save(webhook);
-        }
-    }
-
-    private boolean updatePayoutStatus(Payout payout, Status requestStatus) {
-        rwl.readLock().lock();
-        Status payoutStatus = payout.getStatus();
-
-        if (payout.isFinalStatus()) {
-            if (payoutStatus.equals(requestStatus)) {
-                log.info("Выплата уже находится в статусе {}", payoutStatus);
-                rwl.readLock().unlock();
-                return false;
-            }
-            rwl.readLock().unlock();
-            throw new BadRequestException(
-                    String.format("Не подходящий статус %s для выплаты с ID: %s ", requestStatus, payout.getId())
-            );
-        }
-
-        try {
-            rwl.readLock().unlock();
-            rwl.writeLock().lock();
-
-            if (payout.isFinalStatus()) {
-                if (payout.getStatus().equals(requestStatus)) {
-                    log.info("Выплата уже находится в статусе {}", payout.getStatus());
-                    return false;
-                }
-
-                throw new BadRequestException(
-                        String.format("Не подходящий статус %s для выплаты с ID: %s ", requestStatus, payout.getId())
-                );
-            } else {
-                payout.setStatus(requestStatus);
-                return true;
-            }
-        } finally {
-            rwl.writeLock().unlock();
-        }
-    }
-
-    private boolean updateTransactionStatus(Transaction transaction, Status requestStatus) {
-        rwl.readLock().lock();
         Status transactionStatus = transaction.getStatus();
 
         if (transaction.isFinalStatus()) {
-            rwl.readLock().unlock();
             if (transactionStatus.equals(requestStatus)) {
                 log.info("Пополнение уже находится в статусе {}", transactionStatus);
-                return false;
+                return;
             }
 
             throw new BadRequestException(
                     String.format("Не подходящий статус %s для пополнения с ID: %s ", requestStatus, transaction.getId())
             );
         }
-        try {
-            rwl.readLock().unlock();
-            rwl.writeLock().lock();
 
-            if (transaction.isFinalStatus()) {
-                if (transaction.getStatus().equals(requestStatus)) {
-                    log.info("Пополнение уже находится в статусе {}", transaction.getStatus());
-                    return false;
-                }
-
-                throw new BadRequestException(
-                        String.format("Не подходящий статус %s для пополнения с ID: %s ", requestStatus, transaction.getId())
-                );
-            } else {
-                transaction.setStatus(requestStatus);
-                return true;
-            }
-        } finally {
-            rwl.writeLock().unlock();
-        }
+        transaction.setStatus(requestStatus);
+        Webhook webhook = createWebhook(EventType.TRANSACTION, request);
+        webhookRepository.save(webhook);
     }
 
     private Webhook createWebhook(EventType type, StatusUpdate request) {
